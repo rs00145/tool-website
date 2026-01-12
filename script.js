98f5cf156c91c4c149bfc1470d421efd5a869658
rs00145 <rohit77851@gmail.com>
2026-01-12 20:20:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function toggleMenu() {
+  const menu = document.getElementById("sideMenu");
+  menu.classList.add("open");
+}
 
+function closeMenu() {
+  const menu = document.getElementById("sideMenu");
+  menu.classList.remove("open");
+}
